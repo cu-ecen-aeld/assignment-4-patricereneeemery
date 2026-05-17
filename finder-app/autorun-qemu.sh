a1fe4729a3616da09395332e67977ccd2246fe10
patricereneeemery <patriceemery@yahoo.com>
2026-05-16 23:19:15 -0700

    Assignment 3 Part 2: rootfs build and scripts

--- finder-app/autorun-qemu.sh
+++ finder-app/autorun-qemu.sh
@@ -1,12 +1,29 @@
-#!/bin/sh
-cd $(dirname $0)
-echo "Running test script"
+#!/bin/bash
+# Automatically run finder-test.sh inside QEMU and exit
+
+set -e
+
+# Location of kernel and initramfs
+KERNEL_IMAGE=/tmp/aeld/Image
+INITRAMFS=/tmp/aeld/initramfs.cpio.gz
+
+# Run QEMU and automatically execute finder-test.sh
+qemu-system-arm \
+    -M versatilepb \
+    -m 128M \
+    -nographic \
+    -kernel ${KERNEL_IMAGE} \
+    -append "console=ttyAMA0" \
+    -initrd ${INITRAMFS} \
+    -serial mon:stdio \
+    -no-reboot \
+    -fsdev local,id=fsdev0,path=/tmp/aeld/rootfs,security_model=none \
+    -device virtio-9p-device,fsdev=fsdev0,mount_tag=hostfs \
+<< 'EOF'
+mount -t proc proc /proc
+mount -t sysfs sysfs /sys
+mount -t devtmpfs devtmpfs /dev || true
+cd /home
 ./finder-test.sh
-rc=$?
-if [ ${rc} -eq 0 ]; then
-    echo "Completed with success!!"
-else
-    echo "Completed with failure, failed with rc=${rc}"
-fi
-echo "finder-app execution complete, dropping to terminal"
-/bin/sh
+poweroff -f
+EOF
