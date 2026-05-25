568aa4915213f0366be6fa75046634efc7bce9fb
patricereneeemery <patriceemery@yahoo.com>
2026-05-24 23:05:30 -0700

    Checkpoint: all files up to current debugging state

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -1,124 +1,32 @@
 #!/bin/bash
-# Script outline to install and build kernel.
-# Author: Siddhant Jajoo. Modified by Patrice Emery
+# Fixed version: always use ${HOME}/aeld as OUTDIR
 
-set -e
-set -u
+OUTDIR=${HOME}/aeld
+KERNEL_REPO=${OUTDIR}/linux-stable
+ROOTFS=${OUTDIR}/rootfs
 
-# FIXED: Put OUTDIR inside your repo, not in /home/patrice/aeld
-OUTDIR=/home/patrice/assignments-3-and-later-patricereneeemery/aeld
-
-KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
-KERNEL_VERSION=v5.15.163
-BUSYBOX_VERSION=1_33_1
-FINDER_APP_DIR=$(realpath $(dirname $0))
-export ARCH=arm64
-export CROSS_COMPILE=aarch64-linux-gnu-
-
-if [ $# -lt 1 ]
-then
-    echo "Using default directory ${OUTDIR} for output"
-else
-    OUTDIR=$1
-    echo "Using passed directory ${OUTDIR} for output"
-fi
+echo "Using OUTDIR = ${OUTDIR}"
 
 mkdir -p ${OUTDIR}
+mkdir -p ${ROOTFS}
 
-cd "${OUTDIR}"
-if [ ! -d "${OUTDIR}/linux-stable" ]; then
-    echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
-    git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
+# Build kernel
+if [ ! -d ${KERNEL_REPO} ]; then
+    git clone https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git ${KERNEL_REPO}
 fi
 
-if [ ! -e "${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image" ]; then
-    cd linux-stable
-    echo "Checking out version ${KERNEL_VERSION}"
-    git checkout ${KERNEL_VERSION}
+cd ${KERNEL_REPO}
+git checkout v5.1.10
 
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
-    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
+make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- mrproper
+make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- defconfig
+make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-
 
-    cp arch/${ARCH}/boot/Image ${OUTDIR}/
-fi
+cp arch/arm64/boot/Image ${OUTDIR}/Image
 
-echo "Adding the Image in outdir"
-echo "Creating the staging directory for the root filesystem"
-
-cd "${OUTDIR}"
-if [ -d "${OUTDIR}/rootfs" ]
-then
-    echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
-    sudo rm -rf "${OUTDIR}/rootfs"
-fi
-
-# FIXED: Clean rootfs creation
-mkdir -p "${OUTDIR}/rootfs"
-cd "${OUTDIR}/rootfs"
-mkdir -p bin dev etc etc/init.d home lib lib64 proc sbin sys tmp usr usr/bin usr/lib usr/sbin var var/log
-
-cd "${OUTDIR}"
-if [ ! -d "${OUTDIR}/busybox" ]
-then
-    git clone git://busybox.net/busybox.git
-    cd busybox
-    git checkout ${BUSYBOX_VERSION}
-    make distclean
-    make defconfig
-else
-    cd busybox
-fi
-
-make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
-make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
-
-sudo chown root:root ${OUTDIR}/rootfs/bin/busybox
-sudo chmod 4755 ${OUTDIR}/rootfs/bin/busybox
-
-echo "Library dependencies"
-${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "Shared library"
-
-# FIXED: Correct Ubuntu aarch64 library paths
-cp -a /usr/aarch64-linux-gnu/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib/
-cp -a /usr/aarch64-linux-gnu/lib/libm.so.6 ${OUTDIR}/rootfs/lib64/
-cp -a /usr/aarch64-linux-gnu/lib/libresolv.so.2 ${OUTDIR}/rootfs/lib64/
-cp -a /usr/aarch64-linux-gnu/lib/libc.so.6 ${OUTDIR}/rootfs/lib64/
-
-# Device nodes
-sudo mknod -m 666 ${OUTDIR}/rootfs/dev/null c 1 3
-sudo mknod -m 666 ${OUTDIR}/rootfs/dev/tty c 5 0
-
-# Build writer
-cd "${FINDER_APP_DIR}"
-make clean
-make CROSS_COMPILE=${CROSS_COMPILE}
-cp writer ${OUTDIR}/rootfs/home/
-
-# Copy finder scripts
-cp ${FINDER_APP_DIR}/finder.sh ${OUTDIR}/rootfs/home/
-cp ${FINDER_APP_DIR}/finder-test.sh ${OUTDIR}/rootfs/home/
-cp -r ${FINDER_APP_DIR}/conf ${OUTDIR}/rootfs/home/
-sed -i 's|\.\./conf|conf|g' ${OUTDIR}/rootfs/home/finder-test.sh
-
-# Chown rootfs
-sudo chown -R root:root ${OUTDIR}/rootfs
-
-mkdir -p ${OUTDIR}/rootfs/etc/init.d
-cat << 'EOF' | sudo tee ${OUTDIR}/rootfs/etc/init.d/rcS > /dev/null
-#!/bin/sh
-mount -t proc none /proc
-mount -t sysfs none /sys
-mount -t tmpfs none /tmp
-echo "Boot complete"
-EOF
-
-sudo chmod +x ${OUTDIR}/rootfs/etc/init.d/rcS
-
-# Create initramfs
-cd ${OUTDIR}/rootfs
+# Build initramfs
+cd ${ROOTFS}
 find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
 gzip -f ${OUTDIR}/initramfs.cpio
+
+echo "Kernel and initramfs built in ${OUTDIR}"
--- finder-app/start-qemu-app.sh
+++ finder-app/start-qemu-app.sh
@@ -1,38 +1,30 @@
-# !/bin/bash
-# Script to open qemu terminal.
-# Author: Siddhant Jajoo.
+#!/bin/bash
+# Fixed version: always use ${HOME}/aeld as OUTDIR
 
-set -e
-
-OUTDIR=$1
-
-if [ -z "${OUTDIR}" ]; then
-    OUTDIR=/tmp/aeld
-    echo "No outdir specified, using ${OUTDIR}"
-fi
-
+OUTDIR=${HOME}/aeld
 KERNEL_IMAGE=${OUTDIR}/Image
 INITRD_IMAGE=${OUTDIR}/initramfs.cpio.gz
+
+echo "Using OUTDIR = ${OUTDIR}"
+echo "Kernel      = ${KERNEL_IMAGE}"
+echo "Initramfs   = ${INITRD_IMAGE}"
 
 if [ ! -e ${KERNEL_IMAGE} ]; then
     echo "Missing kernel image at ${KERNEL_IMAGE}"
     exit 1
 fi
+
 if [ ! -e ${INITRD_IMAGE} ]; then
     echo "Missing initrd image at ${INITRD_IMAGE}"
     exit 1
 fi
 
-
-echo "Booting the kernel"
-# See trick at https://superuser.com/a/1412150 to route serial port output to file
 qemu-system-aarch64 \
-        -m 256M \
-        -M virt \
-        -cpu cortex-a53 \
-        -nographic \
-        -smp 1 \
-        -kernel ${KERNEL_IMAGE} \
-        -chardev stdio,id=char0,mux=on,logfile=${OUTDIR}/serial.log,signal=off \
-        -serial chardev:char0 -mon chardev=char0 \
-        -append "rdinit=/home/autorun-qemu.sh console=ttyAMA0" -initrd ${INITRD_IMAGE}
+    -M virt \
+    -cpu cortex-a53 \
+    -nographic \
+    -smp 1 \
+    -m 1024 \
+    -kernel ${KERNEL_IMAGE} \
+    -initrd ${INITRD_IMAGE} \
+    -append "console=ttyAMA0"
